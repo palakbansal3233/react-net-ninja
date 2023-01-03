4601650e3f168947d9afe649b1db07e86fad03af
palakbansal3233 <palakbansal3233@gmail.com>
2023-01-03 11:49:23 +0530

    more changes

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = ({ blogs, handleDelete }) => {
+  return ( 
+    <div className="blog-list">
+      {blogs.map((blog) => {
+        return (
+          <div className='blog-preview' key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+            <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+ 
+export default BlogList;
